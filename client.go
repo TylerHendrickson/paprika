5fc9226cf6e6ac20b91104392995f84e1005e499
TylerHendrickson <1851017+TylerHendrickson@users.noreply.github.com>
2025-11-14 00:35:49 +0000

    Refactor to split client and models

--- client.go
+++ client.go
@@ -1,180 +1,108 @@
 package paprika
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
-const baseUrl = "https://www.paprikaapp.com/api/v1/sync/"
-
-type RecipeItem struct {
-	Hash string `json:"hash,omitempty"`
-	UID  string `json:"uid,omitempty"`
-}
-
-type Result struct {
-	Result *json.RawMessage
-}
-
-type Status struct {
-	Recipes    int `json:"recipes,omitempty"`
-	Pantry     int `json:"pantry,omitempty"`
-	Meals      int `json:"meals,omitempty"`
-	Menus      int `json:"menus,omitempty"`
-	Groceries  int `json:"groceries,omitempty"`
-	Bookmarks  int `json:"bookmarks,omitempty"`
-	Menuitems  int `json:"menuitems,omitempty"`
-	Categories int `json:"categories,omitempty"`
-}
-
-type Bookmark struct {
-	URL       string `json:"url,omitempty"`
-	Title     string `json:"title,omitempty"`
-	UID       string `json:"uid,omitempty"`
-	OrderFlag int    `json:"order_flag,omitempty"`
-}
-
-type Category struct {
-	Name      string `json:"name,omitempty"`
-	UID       string `json:"uid,omitempty"`
-	ParentUID string `json:"parent_uid,omitempty"`
-	OrderFlag int    `json:"order_flag,omitempty"`
-}
-
-type GroceryItem struct {
-	Aisle      string `json:"aisle,omitempty"`
-	UID        string `json:"uid,omitempty"`
-	OrderFlag  int    `json:"order_flag,omitempty"`
-	Recipe     string `json:"recipe,omitempty"`
-	Name       string `json:"name,omitempty"`
-	Purchased  bool   `json:"purchased,omitempty"`
-	RecipeUID  string `json:"recipe_uid,omitempty"`
-	Ingredient string `json:"ingredient,omitempty"`
-}
-
-type Meal struct {
-	UID       string `json:"uid,omitempty"`
-	OrderFlag int    `json:"order_flag,omitempty"`
-	RecipeUID string `json:"recipe_uid,omitempty"`
-	Date      string `json:"date,omitempty"`
-	Type      int    `json:"type,omitempty"`
-	Name      string `json:"name,omitempty"`
-}
-
-type MenuItem struct {
-	Name      string `json:"name,omitempty"`
-	RecipeUID string `json:"recipe_uid,omitempty"`
-	UID       string `json:"uid,omitempty"`
-	MenuUID   string `json:"menu_uid,omitempty"`
-	OrderFlag int    `json:"order_flag,omitempty"`
-}
-
-type PantryItem struct {
-	Aisle      string `json:"aisle,omitempty"`
-	UID        string `json:"uid,omitempty"`
-	Ingredient string `json:"ingredient,omitempty"`
-}
-
-type Recipe struct {
-	Rating          int      `json:"rating,omitempty"`
-	PhotoHash       string   `json:"photo_hash,omitempty"`
-	OnFavorites     bool     `json:"on_favorites,omitempty"`
-	Photo           string   `json:"photo,omitempty"`
-	UID             string   `json:"uid,omitempty"`
-	Scale           string   `json:"scale,omitempty"`
-	Ingredients     string   `json:"ingredients,omitempty"`
-	Source          string   `json:"source,omitempty"`
-	Hash            string   `json:"hash,omitempty"`
-	SourceURL       string   `json:"source_url,omitempty"`
-	Difficulty      string   `json:"difficulty,omitempty"`
-	Categories      []string `json:"categories,omitempty"`
-	PhotoURL        string   `json:"photo_url,omitempty"`
-	CookTime        string   `json:"cook_time,omitempty"`
-	Name            string   `json:"name,omitempty"`
-	Created         string   `json:"created,omitempty"`
-	Notes           string   `json:"notes,omitempty"`
-	ImageURL        string   `json:"image_url,omitempty"`
-	PrepTime        string   `json:"prep_time,omitempty"`
-	Servings        string   `json:"servings,omitempty"`
-	NutritionalInfo string   `json:"nutritional_info,omitempty"`
-	Directions      string   `json:"directions,omitempty"`
-}
+const DefaultBaseURL = "https://www.paprikaapp.com/api/v1/sync/"
 
 type Client struct {
 	username   string
 	password   string
 	httpClient http.Client
+	baseURL    *url.URL
 }
 
-func NewClient(username, password string) (Client, error) {
+func NewClient(username, password string) (*Client, error) {
+	// Must parse DefaultBaseURL
+	u, err := url.Parse(DefaultBaseURL)
+	if err != nil {
+		panic(err)
+	}
+	return NewClientWithURL(username, password, u)
+}
+
+func NewClientWithURL(username, password string, baseURL *url.URL) (*Client, error) {
 	if strings.TrimSpace(username) == "" {
-		return Client{}, fmt.Errorf("username must not be empty")
+		return nil, fmt.Errorf("username must not be empty")
 	}
 
 	if strings.TrimSpace(password) == "" {
-		return Client{}, fmt.Errorf("password must not be empty")
+		return nil, fmt.Errorf("password must not be empty")
 	}
 
-	return Client{
+	return &Client{
 		httpClient: http.Client{},
 		username:   username,
 		password:   password,
+		baseURL:    baseURL,
 	}, nil
 }
 
-func (c Client) Recipes() ([]RecipeItem, error) {
+func (c *Client) Recipes(ctx context.Context) ([]RecipeItem, error) {
 	rs := []RecipeItem{}
-	err := c.get("recipes", &rs)
+	req, err := c.RecipesRequest(ctx)
 	if err != nil {
 		return nil, err
 	}
-
+	err = c.DoRequest(req, &rs)
 	return rs, err
 }
 
-func (c Client) Recipe(uid string) (Recipe, error) {
-	r := Recipe{}
-	err := c.get("recipe/"+uid, &r)
-	if err != nil {
-		return Recipe{}, err
-	}
-
-	return r, err
+func (c *Client) RecipesRequest(ctx context.Context) (*http.Request, error) {
+	return c.prepareGet(ctx, "recipes")
 }
 
-func (c Client) Bookmarks() ([]Bookmark, error) {
-	return nil, nil
+func (c *Client) Recipe(ctx context.Context, uid string) (Recipe, error) {
+	rs := Recipe{}
+	req, err := c.RecipeRequest(ctx, uid)
+	if err != nil {
+		return rs, err
+	}
+	err = c.DoRequest(req, &rs)
+	return rs, err
 }
 
-func (c Client) prepareGet(path string) (*http.Request, error) {
-	req, err := http.NewRequest("GET", baseUrl+path, nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create request for %s: %v", path, err)
-	}
-
-	req.Header = http.Header{
-		"Content-Type": []string{"application/json"},
-	}
-	req.SetBasicAuth(c.username, c.password)
-	return req, nil
+func (c *Client) RecipeRequest(ctx context.Context, uid string) (*http.Request, error) {
+	return c.prepareGet(ctx, "recipe", uid)
 }
 
-func (c Client) get(path string, value interface{}) error {
-	req, err := c.prepareGet(path)
+func (c *Client) Bookmarks(ctx context.Context) ([]Bookmark, error) {
+	rs := []Bookmark{}
+	req, err := c.BookmarksRequest(ctx)
 	if err != nil {
-		return err
+		return rs, err
 	}
+	err = c.DoRequest(req, &rs)
+	return rs, err
+}
 
-	resp, err := c.httpClient.Do(req)
+func (c *Client) BookmarksRequest(ctx context.Context) (*http.Request, error) {
+	return c.prepareGet(ctx, "bookmarks")
+}
+
+func (c *Client) Categories(ctx context.Context) ([]Category, error) {
+	rs := []Category{}
+	req, err := c.CategoriesRequest(ctx)
 	if err != nil {
-		return fmt.Errorf("failed to get %s: %s", path, err)
+		return rs, err
 	}
+	err = c.DoRequest(req, &rs)
+	return rs, err
+}
 
-	defer resp.Body.Close()
-	bodyText, err := ioutil.ReadAll(resp.Body)
+func (c *Client) CategoriesRequest(ctx context.Context) (*http.Request, error) {
+	return c.prepareGet(ctx, "categories")
+}
+
+func (c *Client) UnmarshalWrappedResponse(resp *http.Response, target any) error {
+	bodyText, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return fmt.Errorf("error reading response body: %s", err)
 	}
@@ -183,7 +111,7 @@ func (c Client) get(path string, value interface{}) error {
 		return fmt.Errorf("unexpected status code: %s %s", resp.Status, bodyText)
 	}
 
-	err = unwrapResult(bodyText, value)
+	err = UnwrapResult(bodyText, target)
 	if err != nil {
 		return err
 	}
@@ -191,7 +119,42 @@ func (c Client) get(path string, value interface{}) error {
 	return nil
 }
 
-func unwrapResult(jsonData []byte, value interface{}) error {
+func (c *Client) prepareGet(ctx context.Context, paths ...string) (*http.Request, error) {
+	url := c.baseURL.JoinPath(paths...).String()
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.SetBasicAuth(c.username, c.password)
+	return req, nil
+}
+
+func (c *Client) DoRequest(req *http.Request, value any) error {
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to %s %s: %w", req.Method, req.URL, err)
+	}
+	defer resp.Body.Close()
+
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("error reading response body: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %s %s", resp.Status, bodyText)
+	}
+
+	err = UnwrapResult(bodyText, value)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UnwrapResult(jsonData []byte, value interface{}) error {
 	var wrapper Result
 
 	err := json.Unmarshal(jsonData, &wrapper)
--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
-module github.com/willgorman/go-paprika
+module github.com/TylerHendrickson/paprika
 
-go 1.13
+go 1.25.1
+
+require github.com/stretchr/testify v1.11.1
+
+require (
+	github.com/alecthomas/kong v1.12.1
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rs/zerolog v1.34.0
+	golang.org/x/sys v0.28.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
